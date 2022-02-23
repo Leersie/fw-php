21d761d3ac6102e5cc1b0f7cd29bea3cda2bebbd
22005397 <22005397@etu.unicaen.fr>
2022-02-23 14:38:58 +0100

    oui

--- app/controllers/TodosController.php
+++ app/controllers/TodosController.php
@@ -3,20 +3,42 @@ namespace controllers;
  use Ubiquity\attributes\items\router\Get;
  use Ubiquity\attributes\items\router\Post;
  use Ubiquity\attributes\items\router\Route;
+ use Ubiquity\utils\http\USession;
 
  /**
   * Controller TodosController
   */
 class TodosController extends \controllers\ControllerBase{
 
+    const CACHE_KEY = 'datas/lists/';
+    const EMPTY_LIST_ID='not saved';
+    const LIST_SESSION_KEY='list';
+    const ACTIVE_LIST_SESSION_KEY='active-list';
+
     #[Post(path:'_default')]
 	public function index(){
-		
+		/*$list = USession::get(self::LIST_SESSION_KEY);
+        $this->displayList($list);*/
+        $this->jquery->click('button','$('.elm').toggle();');
+        $this->jquery->renderDefaultView();
 	}
 
     #[Post(path:'todos/add')]
 	public function addElement(){
-		
+        //Récupère une valeur à la position list, retourne un tableau vide si la clé n'existe pas
+        $list=USession::get('list',[]);
+        print_r($list);
+
+        //Modifie une valeur à la position list
+        $list=USession::set(self::ACTIVE_LIST_SESSION_KEY,$list);
+
+        //Teste l'existance de la clé ACTIVE_LIST_SESSION_KEY
+        if(USession::exists(self::ACTIVE_LIST_SESSION_KEY)){
+            echo "ça existe<br/>";
+        }
+
+
+		echo "oui";
 	}
 
      #[Get(path:'todos/delete/{index}')]
@@ -46,8 +68,14 @@ class TodosController extends \controllers\ControllerBase{
 
      #[Get(path:'todos/saveList')]
      public function saveList(){
-
+        echo "save";
      }
 
+
+    #[Route(path:'test')]
+    public function test(){
+        echo "<h1>Réponse Ajax</h1>";
+
+    }
 
  }
